ab2d445e5642a91111248029261b35fe499eeaaf
LaCodon <webmaster@lacodon.de>
2015-11-13 13:34:34 +0100

    did some stuff

---
+++ application/model/Server.php
@@ -0,0 +1,20 @@
+<?php
+
+class Server {
+
+    protected $_ip;
+    protected $_host;
+
+    public function __construct($ip) {
+        $this->_ip = $ip;
+    }
+
+    public function getHost() {
+        if ($this->_host === NULL) {
+            $this->_host = gethostbyaddr($this->_ip);
+        }
+
+        return $this->_host;
+    }
+
+}
---
+++ application/model/User.php
@@ -0,0 +1,11 @@
+<?php
+
+class User {
+    
+    protected $_username;
+    protected $_email;
+    protected $_firstname;
+    protected $_lastname;
+    protected $_server;
+    
+}
