d918f8268f2ea3c97b2a9d1b2c87c7696643f845
LaCodon <fabian.maier@lacodon.de>
2017-05-13 14:45:19 +0200

    Add construct page

---
+++ public/under_construction.php
@@ -0,0 +1,21 @@
+<?php header('Status: 503 Service Temporarily Unavailable', true, 503); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Under construction | MyEnvoy</title>
+        <meta charset="utf-8">
+        <style>
+            *, body, html {margin: 0px;padding: 0px;font-family: 'helvetica', 'Arial';}
+            body {position: fixed;top:0px;bottom: 0px;right: 0px;left: 0px;background: url('img/construct-bg.jpg') no-repeat 40% 10%;color: white;text-align: center;overflow: auto;}
+            img {width: 400px;margin-top: 30px;}
+            h1 {margin-top: 20px;font-size: 40pt;}
+            h2 {margin-top: 40px;letter-spacing: 2px;font-size: 20pt;}
+            @media (max-width: 500px) {img {width: 200px;}}
+        </style>
+    </head>
+    <body>
+        <img src="img/logo/logo.svg">
+        <h1>We're back soon</h1>
+        <h2>New features will be waiting for you!</h2>
+    </body>
+</html>
